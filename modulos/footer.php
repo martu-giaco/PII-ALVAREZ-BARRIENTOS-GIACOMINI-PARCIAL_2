b986cd2c58e4d9fcd60b76f81950244adefadd94
martu G <martina.giacomini@davinci.edu.ar>
2025-06-08 00:36:58 -0300

    un cacho del footer

--- modulos/footer.php
+++ modulos/footer.php
@@ -1,4 +1,20 @@
 <?= $jsBootstrap; ?>
 <footer>
+    <div class="d-flex">
+        <ul>
+            <li><a style="font-weight: 500;" href="?seccion=inicio">Inicio</a></li>
+            <li><a style="font-weight: 500;" href="?seccion=contacto">Contacto</a></li>
+            <li><a style="font-weight: 500;" href="?seccion=alumnas">Alumnas</a></li>
+        </ul>
+        <ul>
+            <li><a style="font-weight: 500;" href="?seccion=productos">Productos</a></li>
+            <ul class="submenu">
+                <li><a href="">iMacs</a></li>
+                <li><a href="">iPads</a></li>
+                <li><a href="">iPhones</a></li>
+                <li><a href="">Airpods</a></li>
+            </ul>
+        </ul>
+    </div>
     <p>©2025 Álvarez Barrientos Giacomini - Programación II</p>
 </footer>

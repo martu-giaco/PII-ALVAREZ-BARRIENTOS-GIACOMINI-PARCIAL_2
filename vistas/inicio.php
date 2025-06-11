5cd4f01b6f4dc93cabdc589e89a22a54643ebf6f
martu G <martina.giacomini@davinci.edu.ar>
2025-06-11 20:51:34 -0300

    inicio wip, footer terminado, imagenes agregadas y mejor organizadas

--- vistas/inicio.php
+++ vistas/inicio.php
@@ -1,1 +1,46 @@
-<p>este sería el inicio/home</p>
+<div class="container-inicio">
+    <section class="header m-3">
+        <h2 class="text-center p-5">Bienvenido a la tienda oficial de Apple Latino América</h2>
+    </section>
+    <section class="d-flex flex-wrap text-center">
+        <div id="ipads-colores" class="m-3">
+            <h3>Un color para cada personalidad</h3>
+            <a href="">Descubrílos</a>
+        </div>
+        <div id="descuento-estudiantes" class="m-3">
+            <h3>Disfrutá de hasta un 30% de descuento</h3>
+            <p>Comprobando que sos estudiante universitario</p>
+        </div>
+    </section>
+    <section></section>
+    <section class="categorias mt-5 mb-5">
+        <h2 class="text-center">Encontrá lo que estás buscando</h2>
+        <p>AGREGAR LOS RESPECTIVOS LINKS DE LAS CATEGORIAS Y DESPUES BORRAR ESTE PARRAFO no me sale hacer un comentario asssdraaaaaaaaaaaaa</p>
+        <div class="d-flex justify-content-center flex-wrap mt-4">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/imagenes/categorias-fotitos/mac-categoria.png" class="card-img-top" alt="Mac">
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary stretched-link">Mac</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/imagenes/categorias-fotitos/iphone-categoria.png" class="card-img-top" alt="iPhone">
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary stretched-link">iPhone</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/imagenes/categorias-fotitos/ipad-categoria.png" class="card-img-top" alt="iPad">
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary stretched-link">iPad</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/imagenes/categorias-fotitos/airpods-categoria.png" class="card-img-top" alt="airpods">
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary stretched-link">Airpods</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>

6532b9fddfa7d7e2983ad703f6232df7fa29e63f
martu G <martina.giacomini@davinci.edu.ar>
2025-06-16 15:18:03 -0300

    detalles variows

--- vistas/inicio.php
+++ vistas/inicio.php
@@ -20,7 +20,7 @@
     </section>
     <section class="categorias mt-5 pt-5 mb-5">
         <h2 class="text-center">Encontrá lo que estás buscando</h2>
-        <!-- AGREGAR LOS RESPECTIVOS LINKS DE LAS CATEGORIAS Y DESPUES BORRAR ESTE PARRAFO no me sale hacer un comentario asssdraaaaaaaaaaaaa -->
+        <!-- AGREGAR LOS RESPECTIVOS LINKS DE LAS CATEGORIAS Y DESPUES BORRAR ESTE PARRAFO no me sale hacer un comentario, asssdraaaaaaaaaaaaa -->
         <div class="d-flex justify-content-center flex-wrap mt-4">
             <div class="card" style="width: 18rem;">
                 <img src="assets/imagenes/categorias-fotitos/mac-categoria.png" class="card-img-top" alt="Mac">

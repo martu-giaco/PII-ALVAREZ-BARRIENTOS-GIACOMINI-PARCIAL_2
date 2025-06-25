1ae18110b1b1d5950eafd3ae672de0a9b8e33382
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-06-25 09:47:14 -0300

    arreglo bdd y fotos prods

--- vistas/productos.php
+++ vistas/productos.php
@@ -16,7 +16,7 @@ $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php foreach($productos as $producto): ?>
         <div class="col-md-4 mb-4">
             <div class="card h-100">
-            <img src="assets/imagenes/<?php echo $producto['imagen']; ?>" class="card-img-top" alt="<?php echo ($producto['nombre']); ?>">
+            <img src="assets/imagenes/prods/<?php echo $producto['imagen']; ?>" class="card-img-top" alt="<?php echo ($producto['nombre']); ?>">
             <div class="card-body">
                 <h5 class="card-title"><?php echo $producto['nombre']; ?></h5>
                 <p class="card-text">$<?php echo$producto['precio'], 2, ',', '.'; ?></p>

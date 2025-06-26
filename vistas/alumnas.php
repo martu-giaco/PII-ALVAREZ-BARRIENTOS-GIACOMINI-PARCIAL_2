4aaf236c59f13a649c2747df23c393360fd2f805
martu G <martina.giacomini@davinci.edu.ar>
2025-06-26 13:00:08 -0300

    seccion alumnas (cada una agregue su info!!) hecha, productos css hecho

---
+++ vistas/alumnas.php
@@ -0,0 +1,28 @@
+<div class="container-alumnas d-flex justify-content-center flex-wrap align-items-start m-5 p-5">
+    <div class="mx-5" id="alumna">
+        <img src="" alt="Martina Álvarez">
+        <div>
+            <h2>Martina Álvarez</h2>
+            <p>cumpleaños</p>
+            <p>martina.alvarez@davinci.edu.ar</p>
+        </div>
+    </div>
+
+    <div class="mx-5 px-5" id="alumna">
+        <img src="" alt="Abril Barrientos">
+        <div>
+            <h2>Abril Barrientos</h2>
+            <p>cumpleaños</p>
+            <p>abril.barrientos@davinci.edu.ar</p>
+        </div>
+    </div>
+
+    <div class="mx-5" id="alumna">
+        <img src="assets/imagenes/alumnas/martu-g.png" alt="Martina Giacomini">
+        <div>
+            <h2>Martina Giacomini</h2>
+            <p>15/03/2006</p>
+            <p>martina.giacomini@davinci.edu.ar</p>
+        </div>
+    </div>
+</div>
--- vistas/productos.php
+++ vistas/productos.php
@@ -1,4 +1,4 @@
-
+<div class="container-productos m-5">
 <?php
 require_once 'classes/Conexion.php';
 
@@ -12,18 +12,19 @@ $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <main class="container my-5">
     <h2 class="mb-4">Productos</h2>
-    <div class="row">
+    <div class="row justify-content-center">
         <?php foreach($productos as $producto): ?>
-        <div class="col-md-4 mb-4">
+        <div class="col-md-4 mb-5">
             <div class="card h-100">
-            <img src="assets/imagenes/prods/<?php echo $producto['imagen']; ?>" class="card-img-top" alt="<?php echo ($producto['nombre']); ?>">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $producto['nombre']; ?></h5>
-                <p class="card-text">$<?php echo$producto['precio'], 2, ',', '.'; ?></p>
-            </div>
+            <img src="assets/imagenes/prods/<?php echo $producto['imagen']; ?>" class="card-img-top p-5" alt="<?php echo ($producto['nombre']); ?>">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $producto['nombre']; ?></h5>
+                    <p class="card-text">$<?php echo$producto['precio']?></p>
+                    <a href="?sec=producto&id=<?= $producto['id']; ?>" class="btn mt-3">Ver Detalles</a>
+                </div>
             </div>
         </div>
         <?php endforeach; ?>
     </div>
 </main>
-
+</div>

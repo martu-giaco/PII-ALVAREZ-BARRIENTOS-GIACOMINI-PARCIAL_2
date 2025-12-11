62b68f71c9e882570869d8a97ea8f9a08a1616c9
martualvaarez <martina.alvarez@davinci.edu.ar>
2025-12-11 11:47:35 -0300

    se me chispoteo la linea 180

--- classes/Compra.php
+++ classes/Compra.php
@@ -180,7 +180,7 @@ public function cargarDetalles(): void
         WHERE cd.id_compra = :id_compra
     ");
 
-    $stmt->execute(['id' => $this->id_compra]);
+    $stmt->execute(['id_compra' => $this->id_compra]);
 
     // Guardamos en la propiedad detalles
     $this->detalles = $stmt->fetchAll(PDO::FETCH_ASSOC);
